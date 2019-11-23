0b530a146aa327677db349cd8699f644a1a1c4f8
FeliciaMarlove <missflorencemary@hotmail.com>
2019-11-23 16:15:53 +0100

    Persistence dependencies added. Models > DefiEntity added.

---
+++ src/main/java/be/technofuturtic/javaangularapp/models/DefiEntity.java
@@ -0,0 +1,62 @@
+package be.technofuturtic.javaangularapp.models;
+
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Defi", schema = "public", catalog = "dbslide")
+public class DefiEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "id_defi")
+    private Integer idDefi;
+
+    @Column(name = "nom", nullable = false, unique = true)
+    private String nomDefi;
+
+    @Column(name = "description", nullable = false)
+    private String descDefi;
+
+    @Column(name = "infobulle")
+    private String infobulleDefi;
+
+    @Column(name = "is_active", nullable = false)
+    private boolean isActiveDefi;
+
+    public Integer getIdDefi() {
+        return idDefi;
+    }
+
+    public String getNomDefi() {
+        return nomDefi;
+    }
+
+    public void setNomDefi(String nomDefi) {
+        this.nomDefi = nomDefi;
+    }
+
+    public String getDescDefi() {
+        return descDefi;
+    }
+
+    public void setDescDefi(String descDefi) {
+        this.descDefi = descDefi;
+    }
+
+    public String getInfobulleDefi() {
+        return infobulleDefi;
+    }
+
+    public void setInfobulleDefi(String infobulleDefi) {
+        this.infobulleDefi = infobulleDefi;
+    }
+
+    public boolean isActiveDefi() {
+        return isActiveDefi;
+    }
+
+    public void setActiveDefi(boolean activeDefi) {
+        isActiveDefi = activeDefi;
+    }
+}
