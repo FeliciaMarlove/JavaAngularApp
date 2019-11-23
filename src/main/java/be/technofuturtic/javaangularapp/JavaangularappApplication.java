92be7071421c00c6f2ec9c03de73df9edc68a799
FeliciaMarlove <missflorencemary@hotmail.com>
2019-11-23 15:27:01 +0100

    Initial commit

---
+++ src/main/java/be/technofuturtic/javaangularapp/JavaangularappApplication.java
@@ -0,0 +1,13 @@
+package be.technofuturtic.javaangularapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaangularappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaangularappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/technofuturtic/javaangularapp/JavaangularappApplicationTests.java
@@ -0,0 +1,13 @@
+package be.technofuturtic.javaangularapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaangularappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
